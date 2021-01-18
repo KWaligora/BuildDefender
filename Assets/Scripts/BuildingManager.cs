8fba4875eeaa94a0ee8bf6ff76e0ba1bc200b715
unknown <Shasto>
2021-01-18 14:25:52 +0100

    Create Scriptable object BuildingType

---
+++ Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform PFWoodHarvester;
+
+    private Camera mainCamera;   
+
+    // On game begin
+    private void Start()
+    {
+        mainCamera = Camera.main;   
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(PFWoodHarvester, GetMouseWorldPosition(), Quaternion.identity);
+        }
+    }
+
+    // returning world mouse position
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPosition.z = 0.0f;
+        return mouseWorldPosition;
+    }
+}
