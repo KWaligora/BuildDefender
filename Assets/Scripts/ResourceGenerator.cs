d4ce0962126d722e076be7e787dd4f0da11595a3
unknown <Shasto>
2021-01-19 11:05:29 +0100

    Add resource spawn timers

--- Assets/Scripts/ResourceGenerator.cs
+++ Assets/Scripts/ResourceGenerator.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class ResourceGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float timer;
+    private float timerMax;
+    private BuildingTypeSO buildingType;
+
+    private void Awake()
     {
-        
+        buildingType = GetComponent<BuildingTypeHolder>().buildingType;
+        timerMax = buildingType.resourceGeneratorData.timerMax;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+        if (timer < 0) 
+        {
+            timer += timerMax;
+            Debug.Log(buildingType.resourceGeneratorData.resourceType.nameString);
+        }
+                  
     }
 }
