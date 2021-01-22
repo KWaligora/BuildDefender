807871e4485a7cdf77d4b11b64df0ecdcca815b3
unknown <Shasto>
2021-01-22 13:19:00 +0100

    Add cinemachin + script

---
+++ Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraHandler : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
+
+    private float orthographicSize;
+    private float targetOrthograficSize;
+
+    private void Start()
+    {
+        orthographicSize = cinemachineVirtualCamera.m_Lens.OrthographicSize;
+        targetOrthograficSize = orthographicSize;
+    }
+
+    void Update()
+    {
+        HandleMovement();
+        HandleZoom();
+    }
+
+    private void HandleMovement()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector2 moveDir = new Vector2(x, y).normalized;
+        float moveSpeed = 30f;
+        transform.position += (Vector3)moveDir * moveSpeed * Time.deltaTime;
+    }
+
+    private void HandleZoom()
+    {
+        float zoomAmount = 2f;
+        targetOrthograficSize -= Input.mouseScrollDelta.y * zoomAmount;
+
+        float minOrthographicSize = 10;
+        float maxOrthographicSize = 30;
+        targetOrthograficSize = Mathf.Clamp(targetOrthograficSize, minOrthographicSize, maxOrthographicSize);
+
+        float zoomSpeed = 5f;
+        orthographicSize = Mathf.Lerp(orthographicSize, targetOrthograficSize, zoomSpeed * Time.deltaTime);
+        cinemachineVirtualCamera.m_Lens.OrthographicSize = orthographicSize;
+    }
+}
