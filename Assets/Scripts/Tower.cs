e7e092457f61f6958e40c164ac60bd388a621e8e
unknown <Shasto>
2021-02-08 21:58:12 +0100

    Add Tower class

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+
+    private Transform targetTransform;
+
+    private float lookForTargetTimer;
+    private float lookForTargetTimerMax = .2f;
+
+    private void Update()
+    {
+        HandleTargeting();
+    }
+
+    private void LookForTargets()
+    {
+        float targetMaxRadius = 10f;
+        Collider2D[] collider2DArray = Physics2D.OverlapCircleAll(transform.position, targetMaxRadius);
+
+        foreach (Collider2D collider2D in collider2DArray)
+        {
+            Enemy enemy = collider2D.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                if (targetTransform == null)
+                    targetTransform = enemy.transform;
+                else
+                {
+                    if (Vector3.Distance(transform.position, enemy.transform.position) <
+                        Vector3.Distance(transform.position, targetTransform.position))
+                    {
+                        targetTransform = enemy.transform;
+                    }
+                }
+            }
+        }      
+    }
+
+    private void HandleTargeting()
+    {
+        lookForTargetTimer -= Time.deltaTime;
+        if (lookForTargetTimer <= 0f)
+        {
+            lookForTargetTimer += lookForTargetTimerMax;
+            LookForTargets();
+        }
+    }
+}
